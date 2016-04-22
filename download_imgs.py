392297061411da609043926433411f4bee83207b
zted <zhangteddy22@gmail.com>
2016-04-22 15:51:34 +0200

    Working script to include arguments

--- download_imgs.py
+++ download_imgs.py
@@ -4,27 +4,28 @@ import os
 from imsearchtools.utils import result_page_gen
 import sys
 
+
 def test_callback(out_dict):
     import json
     # print json.dumps(out_dict)
     time.sleep(0.2)
 
 
-def download_images(wordsfile, dir_root):
+def download_images(wordlist, dir_root, numresults):
     opts = ist.process.ImageProcessorSettings()
     opts.conversion['max_height'] = 256
     opts.conversion['max_width'] = 256
     opts.conversion['format'] = 'jpg'
     imgetter = ist.process.ImageGetter(opts=opts)
     searcher = ist.query.BingAPISearch()
-    for query_string in wordsfile:
+    for n,query_string in enumerate(wordlist):
         print 'Executing Bing Image Search...'
         t = time.time()
         # example of querying for image URLs using Google Web engine
         # TODO: exception handler for queries not found, implement handler,
         # perhaps because internet disconnected that results cannot be retrieved.
         # put a counter here or something
-        results = searcher.query(query_string, num_results=10)
+        results = searcher.query(query_string, num_results=numresults)
         results_copy = results[:]
 
         print 'Retrieved %d result URLs in %f seconds' % (len(results), (time.time() - t))
@@ -41,18 +42,21 @@ def download_images(wordsfile, dir_root):
         #    if it is not required, the parameter can simply be removed
         output_fns = imgetter.process_urls(results, outdir, test_callback)
         print 'Downloaded %d results in %f seconds' % (len(output_fns), (time.time() - t))
-
+        time.sleep(0.2) # give some time before next query
+        print("Processed {} words".format(n+1))
 
 if __name__ == "__main__":
+
     working_directory = sys.argv[1]
     if not(os.path.isdir(working_directory)):
         raise EnvironmentError("Directory entered is not a valid path")
     words_file = sys.argv[2]
-    uniqueWords = set([])
+    num_results = 500
+    word_list = []
     with open(words_file, 'r') as f:
         for line in f:
             word = line.rstrip('\n')
             print(word)
-            uniqueWords.add(word)
-    print(len(uniqueWords))
-    # download_images(words_file, working_directory)
+            word_list.append(word)
+    print(len(word_list))
+    download_images(word_list, working_directory, num_results)
