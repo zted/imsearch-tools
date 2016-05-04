3e858ce237b3b9706a0499858657974f9310b0be
zted <zhangteddy22@gmail.com>
2016-05-04 15:51:16 +0200

    Adding print statements and timer

--- download_from_html.py
+++ download_from_html.py
@@ -8,6 +8,8 @@ import os
 def download_images(top_directory):
     all_words = os.listdir(top_directory)
     for word in all_words:
+        print('Downloading images for "{}"'.format(word))
+        t = time.time()
         download_folder = '{}/{}'.format(top_directory, word)
         htmlfile = '{}/{}.html'.format(download_folder, word)
         f = open(htmlfile, 'r')
@@ -18,8 +20,13 @@ def download_images(top_directory):
         for n, img_link in enumerate(img_links):
             address = img_link.img['src']
             filepath = '{}/{}.jpg'.format(download_folder, n)
-            urllib.urlretrieve(address, filepath)
+            try:
+                urllib.urlretrieve(address, filepath)
+            except IOError:
+                continue
             time.sleep(0.2)
+        print('Time taken to download all images in this folder: {}'.format(time.time() - t))
+
 
 if __name__ == "__main__":
 
